0a062742cb089bd2a31c0d279dc1194eb8e7f046
Sergey234 <mostov.sergey88@gmail.com>
2022-11-03 15:30:32 +0300

    stage0-module4-conditions-task3

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        if (character == 'A' || character == 'a' || character == 'E' || character == 'e' || character == 'I' || character == 'i' || character == 'O'
+                || character == 'o' || character == 'U' || character == 'u' || character == 'Y' || character == 'y') {
+            System.out.println("Vowel");
+        } else if (character == 'B' || character == 'b' || character == 'C' || character == 'c' || character == 'D' || character == 'd' || character == 'F'
+                || character == 'f' || character == 'G' || character == 'g' || character == 'H' || character == 'h' || character == 'J' || character == 'j'
+                ||  character == 'K'|| character == 'k' || character == 'L' || character == 'l' || character == 'M' || character == 'm' || character == 'N'
+                || character == 'n' || character == 'Q'|| character == 'q' || character == 'R' || character == 'r' || character == 'S' || character == 's'
+                || character == 'T' || character == 't' || character == 'V' || character == 'v' || character == 'W' || character == 'w' ||character == 'X'
+                || character == 'x' || character == 'Z' || character == 'z') {
+            System.out.println("Consonant");
+        } else {
+            System.out.println("wrong alphabet!");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
@@ -2,5 +2,17 @@ package school.mjc.stage0.conditions.task3;
 
 public class IsEnglishSymbolDeterminer {
     public void isEnglishSymbol(char symbol) {
+        if (symbol == 'A' || symbol == 'a' || symbol == 'B' || symbol == 'b' || symbol == 'C' || symbol == 'c' || symbol == 'D' || symbol == 'd'
+        || symbol == 'E' || symbol == 'e' || symbol == 'F' || symbol == 'f' || symbol == 'G' || symbol == 'g' || symbol == 'H' || symbol == 'h'
+        || symbol == 'I' || symbol == 'i' || symbol == 'J' || symbol == 'j' || symbol == 'K' || symbol == 'k' || symbol == 'L' || symbol == 'l'
+        || symbol == 'M' || symbol == 'm' || symbol == 'N' || symbol == 'n' || symbol == 'O' || symbol == 'o' || symbol == 'P' || symbol == 'p'
+        || symbol == 'Q' || symbol == 'q' || symbol == 'R' || symbol == 'r' || symbol == 'S' || symbol == 's' || symbol == 'T' || symbol == 't'
+        || symbol == 'U' || symbol == 'u' || symbol == 'V' || symbol == 'v' || symbol == 'W' || symbol == 'w' || symbol == 'X' || symbol == 'x'
+        || symbol == 'Y' || symbol == 'y' || symbol == 'Z' || symbol == 'z') {
+            System.out.println("English");
+        } else {
+            System.out.println("Non English");
+        }
+
     }
 }
